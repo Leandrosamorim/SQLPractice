f834c4bb8fd3ec517520adc73cee55c6482a7ba6
Leandro Amorim <leandro_samorim@Hotmail.com>
2020-02-23 04:55:08 -0300

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication6.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <table>
+            <tr>
+                <td>Nome</td>
+                <td><asp:TextBox ID="nome" runat="server"></asp:TextBox></td>
+            </tr>
+             <tr>
+                <td>Sobrenome</td>
+                <td><asp:TextBox ID="sobrenome" runat="server"></asp:TextBox></td>
+            </tr>
+             <tr>
+                <td>Nota 1</td>
+                <td><asp:TextBox ID="nota1" runat="server"></asp:TextBox></td>
+            </tr>
+             <tr>
+                <td>Nota 2</td>
+                <td><asp:TextBox ID="nota2" runat="server"></asp:TextBox></td>
+            </tr>
+             <tr>
+                <td>Nota 3 </td>
+                <td><asp:TextBox ID="nota3" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td colspan ="2" align="center">
+                     <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />
+                     <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="Button2_Click" />
+                     <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click" />
+                     <asp:Button ID="Button4" runat="server" Text="View" OnClick="Button4_Click" />
+                </td>
+            </tr>
+              <tr>
+                <td>Editar por ID</td>
+                <td><asp:TextBox ID="editId" runat="server"></asp:TextBox></td>
+            </tr>
+        </table>
+
+        <br />
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True;Connect Timeout=30");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+            DisplayData();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Insert into Table1 values('" + nome.Text + "','" + sobrenome.Text + "','" + Convert.ToDecimal(nota1.Text) + "','" + Convert.ToDecimal(nota2.Text) + "','" + Convert.ToDecimal(nota3.Text) +"','" + ((Convert.ToDecimal(nota1.Text) + Convert.ToDecimal(nota2.Text) + Convert.ToDecimal(nota3.Text))/3) + "')";
+            cmd.ExecuteNonQuery();
+
+            nome.Text = "";
+            sobrenome.Text = "";
+            nota1.Text = "";
+            nota2.Text = "";
+            nota3.Text = "";
+            DisplayData();
+        }
+
+        public void DisplayData()
+        {
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText ="select * from Table1";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from Table1 where nome='"+ nome.Text +"'";
+            cmd.ExecuteNonQuery();
+
+            nome.Text = "";
+            DisplayData();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update Table1 set nome='"+nome.Text+"',sobrenome='"+sobrenome.Text+"',nota1='"+nota1.Text+"',nota2='"+nota2.Text+"',nota3='"+ nota3.Text+"',media='"+ ((Convert.ToDecimal(nota1.Text) + Convert.ToDecimal(nota2.Text) + Convert.ToDecimal(nota3.Text)) / 3)+"' where id=" + Convert.ToInt32(editId.Text) + "";
+            cmd.ExecuteNonQuery();
+
+            nome.Text = "";
+            sobrenome.Text = "";
+            nota1.Text = "";
+            nota2.Text = "";
+            nota3.Text = "";
+            DisplayData();
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Table1 where media > 6";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
